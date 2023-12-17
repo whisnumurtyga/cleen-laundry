dd5a698eaa15253058b8bed72d888e4f8eec18f7
aliakbar121 <69615734+aliakbar121@users.noreply.github.com>
2023-12-17 19:40:24 +0700

    First commit
    CartScreen

--- App.js
+++ App.js
@@ -1,46 +1,101 @@
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from "react-native-reanimated";
-import { View, Button } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native';
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
+const CLOTHING_ITEMS = [
+  {
+    id: 1,
+    image: require('./images/22.jpg'),
+    name: 'Kaos',
+    price: 2000,
+    quantity: 0, // Initialize quantity
+  },
+  {
+    id: 2,
+    image: require('./images/11.jpg'),
+    name: 'Kemeja',
+    price: 3000,
+    quantity: 0, // Initialize quantity
+  },
+  {
+    id: 3,
+    image: require('./images/3.png'),
+    name: 'Celana',
+    price: 3000,
+    quantity: 0, // Initialize quantity
+  },
+  {
+    id: 4,
+    image: require('./images/4.png'),
+    name: 'Kemeja',
+    price: 3000,
+    quantity: 0, // Initialize quantity
+  },
+  {
+    id: 5,
+    image: require('./images/5.png'),
+    name: 'Kemeja',
+    price: 3000,
+    quantity: 0, // Initialize quantity
+  },
+];
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
+const CartScreen = () => {
+  const [cartItems, setCartItems] = useState(CLOTHING_ITEMS);
+
+  const getTotalPrice = () => cartItems.reduce((sum, item) => {
+    if (item.quantity > 0) {
+      return sum + item.price * item.quantity;
+    } else {
+      return sum;
+    }
+  }, 0);
+
+  const handleRemoveItem = (itemId) => {
+    setCartItems(cartItems.filter((item) => item.id !== itemId));
   };
 
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
+  const handleQuantityChange = (itemId, newQuantity) => {
+    setCartItems(
+      cartItems.map((item) =>
+        item.id === itemId ? { ...item, quantity: newQuantity } : item
+      )
+    );
+  };
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
+  const renderItem = ({ item }) => (
+    <View style={{ flexDirection: 'row', padding: 10 }}>
+      <Image source={item.image} style={{ width: 50, height: 50 }} />
+      <View style={{ marginLeft: 10 }}>
+        <Text>{item.name}</Text>
+        <Text>Rp {item.price} /item</Text>
+        
+      </View  >
+          <TouchableOpacity onPress={() => handleQuantityChange(item.id, item.quantity - 1)} style={{ marginLeft: 'auto' }}>
+            <Text>-</Text>
+          </TouchableOpacity>
+            <Text style={{ paddingHorizontal: 10 }}>{item.quantity}</Text>
+          <TouchableOpacity onPress={() => handleQuantityChange(item.id, item.quantity + 1)} style={{ marginLeft: 'auto' }}>
+            <Text>+</Text>
+          </TouchableOpacity>
     </View>
   );
-}
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Text style={{ paddingTop: 30, fontSize: 20, fontWeight: 'bold' }}>Keranjang Pakaian</Text>
+      <FlatList
+        data={cartItems}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()} // Ensure key is a string
+      />
+      <View style={{ padding: 10 }}>
+        <Text>Total Harga: Rp {getTotalPrice()}</Text>
+        <TouchableOpacity style={{ backgroundColor: '#2E6B60', padding: 10 }}>
+          <Text style={{ fontSize: 20 ,color: 'white' }}>Schedule</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default CartScreen;
---
+++ Screens/CartScreen.js
@@ -0,0 +1,100 @@
+
+const CLOTHING_ITEMS = [
+    {
+      id: 1,
+      image: require('./images/22.jpg'),
+      name: 'Kaos',
+      price: 2000,
+      quantity: 0, // Initialize quantity
+    },
+    {
+      id: 2,
+      image: require('./images/11.jpg'),
+      name: 'Kemeja',
+      price: 3000,
+      quantity: 0, // Initialize quantity
+    },
+    {
+      id: 3,
+      image: require('./images/3.png'),
+      name: 'Celana',
+      price: 3000,
+      quantity: 0, // Initialize quantity
+    },
+    {
+      id: 4,
+      image: require('./images/4.png'),
+      name: 'Kemeja',
+      price: 3000,
+      quantity: 0, // Initialize quantity
+    },
+    {
+      id: 5,
+      image: require('./images/5.png'),
+      name: 'Kemeja',
+      price: 3000,
+      quantity: 0, // Initialize quantity
+    },
+  ];
+  
+  const CartScreen = () => {
+    const [cartItems, setCartItems] = useState(CLOTHING_ITEMS);
+  
+    const getTotalPrice = () => cartItems.reduce((sum, item) => {
+      if (item.quantity > 0) {
+        return sum + item.price * item.quantity;
+      } else {
+        return sum;
+      }
+    }, 0);
+  
+    const handleRemoveItem = (itemId) => {
+      setCartItems(cartItems.filter((item) => item.id !== itemId));
+    };
+  
+    const handleQuantityChange = (itemId, newQuantity) => {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === itemId ? { ...item, quantity: newQuantity } : item
+        )
+      );
+    };
+  
+    const renderItem = ({ item }) => (
+      <View style={{ flexDirection: 'row', padding: 10 }}>
+        <Image source={item.image} style={{ width: 50, height: 50 }} />
+        <View style={{ marginLeft: 10 }}>
+          <Text>{item.name}</Text>
+          <Text>Rp {item.price} /item</Text>
+          
+        </View  >
+            <TouchableOpacity onPress={() => handleQuantityChange(item.id, item.quantity - 1)} style={{ marginLeft: 'auto' }}>
+              <Text>-</Text>
+            </TouchableOpacity>
+              <Text style={{ paddingHorizontal: 10 }}>{item.quantity}</Text>
+            <TouchableOpacity onPress={() => handleQuantityChange(item.id, item.quantity + 1)} style={{ marginLeft: 'auto' }}>
+              <Text>+</Text>
+            </TouchableOpacity>
+      </View>
+    );
+  
+    return (
+      <View style={{ flex: 1 }}>
+        <Text style={{ paddingTop: 30, fontSize: 20, fontWeight: 'bold' }}>Keranjang Pakaian</Text>
+        <FlatList
+          data={cartItems}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()} // Ensure key is a string
+        />
+        <View style={{ padding: 10 }}>
+          <Text>Total Harga: Rp {getTotalPrice()}</Text>
+          <TouchableOpacity style={{ backgroundColor: '#2E6B60', padding: 10 }}>
+            <Text style={{ fontSize: 20 ,color: 'white' }}>Schedule</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+  
+  export default CartScreen;
+  
