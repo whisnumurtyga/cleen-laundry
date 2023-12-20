8a95c3c87bc8a341118fa9d4320a97dfd7dc2105
whisnumurtyga <whisnumurty.galih.21@student.if.ittelkom-sby.ac.id>
2023-12-20 14:07:29 +0700

    :v

--- App.js
+++ App.js
@@ -1,40 +1,39 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import { Signin, Signup, Welcome } from "./screens";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import {Signin, Signup, Welcome} from "./screens";
+import {NativeBaseProvider, Box} from "native-base";
+
 // import GlobalFont from 'react-native-global-font';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  // GlobalFont.applyGlobal('Montserrat-Regular');
-  
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName='Welcome'
-      >
-        <Stack.Screen
-          name="Welcome"
-          component={Welcome}
-          options={{
-            headerShown: false
-          }}
-        />
-        <Stack.Screen
-          name="Signin"
-          component={Signin}
-          options={{
-            headerShown: false
-          }}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={Signup}
-          options={{
-            headerShown: false
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    // GlobalFont.applyGlobal('Montserrat-Regular');
+
+    return (
+        <NativeBaseProvider>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName='Welcome'>
+                    <Stack.Screen
+                        name="Welcome"
+                        component={Welcome}
+                        options={{
+                            headerShown: false
+                        }}/>
+                    <Stack.Screen
+                        name="Signin"
+                        component={Signin}
+                        options={{
+                            headerShown: false
+                        }}/>
+                    <Stack.Screen
+                        name="Signup"
+                        component={Signup}
+                        options={{
+                            headerShown: false
+                        }}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        </NativeBaseProvider>
+    );
 }
--- screens/signup.js
+++ screens/signup.js
@@ -1,91 +1,154 @@
-import { View, Text, Image, Pressable, TextInput, TouchableOpacity, StyleSheet, Dimensions  } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+    View,
+    Text,
+    Image,
+    Pressable,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions
+} from 'react-native';
+import React, {useState} from 'react';
 import COLORS from '../constants/colors';
-import { LogoWide, InputA } from '../components';
+import {LogoWide, InputA, FilledButton} from '../components';
+import {Tabs, Tab} from 'native-base';
 
-
-const { height: screenHeight } = Dimensions.get('window');
-const { width: screenWidth } = Dimensions.get('window');
+const {height: screenHeight} = Dimensions.get('window');
+const {width: screenWidth} = Dimensions.get('window');
 const heightPercentage = 0.8; // 80% dari tinggi layar
 
-const Signup = ({ navigation }) => {
+const Signup = ({navigation}) => {
     const [isPasswordShown, setIsPasswordShown] = useState(false);
     const [isChecked, setIsChecked] = useState(false);
+    const [activeTab, setActiveTab] = useState(0); // Untuk melacak tab yang aktif
+    const [isSignUpActive, setIsSignUpActive] = useState(true); // Defaultnya Sign Up aktif
 
-    return (
+	return (
         <View style={[styles.bg]}>
-			<LogoWide></LogoWide>
-			<View style={styles.centeredContainer}>
-				<Text style={styles.subTitle}>Click for Clean</Text>
+            <LogoWide></LogoWide>
+            <View style={styles.centeredContainer}>
+                <Text style={styles.subTitle}>Click for Clean</Text>
+            </View>
+            <View style={[styles.rectangle]}>
+                <View style={[styles.inputContainer, isSignUpActive ? null : styles.signInInputContainer]}>
+                    {isSignUpActive ? (
+                        <>
+                            <InputA
+                                iconSource={require('../assets/icon/full-name.png')}
+                                placeholder='Full Name'
+                            />
+                            <InputA
+                                iconSource={require('../assets/icon/email.png')}
+                                placeholder='Email'
+                            />
+                            <InputA
+                                iconSource={require('../assets/icon/phone-number.png')}
+                                placeholder='Phone Number'
+                            />
+                            <InputA
+                                iconSource={require('../assets/icon/password.png')}
+                                placeholder='Password'
+                            />
+                        </>
+                    ) : (
+                        <>
+                            <InputA
+                                iconSource={require('../assets/icon/email.png')}
+                                placeholder='Email'
+                            />
+                            <InputA
+                                iconSource={require('../assets/icon/password.png')}
+                                placeholder='Password'
+                            />
+                        </>
+                    )}
+                </View>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => setIsSignUpActive(!isSignUpActive)}
+                >
+                    <Text style={styles.buttonText}>
+                        {isSignUpActive ? 'Sign In' : 'Sign Up'}
+                    </Text>
+                </TouchableOpacity>
+			<View style={styles.bottomCenteredContainer}>
+				<FilledButton
+					textColor={COLORS.white}
+					buttonColor={"#2E6B60"}
+					borderColor={"#2E6B60"}
+					title={isSignUpActive ? ("Sign Up") : ("Sign In")}
+				/>
 			</View>
-			<View style={[styles.rectangle]}>
-				<View style={styles.inputGroup}>
-					<InputA 
-						iconSource={require('../assets/icon/full-name.png')}
-						placeholder='Full Name'
-					/>
-				</View>
 			</View>
-			<View>
-			</View>
-            {/* <Text style={[styles.leenTitle]}>
-				leen
-			</Text>
-			<Image
-				source={require('../assets/splash/logo.png')}
-				style={[styles.logoImage]}
-			/>
-			<View style={styles.centeredContainer}>
-				<Text style={styles.subTitle}>Click for Clean</Text>
-			</View> */}
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
-	inputGroup: {
-		top: 100
-	},	
-	bg: {
+    inputContainer: {
+        flexDirection: 'column', // Menyusun elemen secara vertikal
+        alignItems: 'center', // Mengatur posisi secara vertikal di tengah
+        justifyContent: 'center', // Mengatur jarak antar input
+        height: 200, // Atur tinggi sesuai dengan kebutuhan
+        top: 100
+    },
+	signInInputContainer: {
+        height: 100, // Atur tinggi sesuai dengan kebutuhan pada mode Sign In
+    },
+    bg: {
         backgroundColor: COLORS.primary,
-        flex: 1,
+        flex: 1
     },
     leenTitle: {
-		top: 80,
-		left: 180,
-		color: COLORS.white,
-		fontSize: 80,
-		transform: [{ translateX: 0 }],
-	},
-	logoImage: {
-		left: 96,
-		top: -23,
-		width: 82,
-		height: 82,
-		resizeMode: 'contain',
-		transform: [{ translateX: 0 }],
-	},
+        top: 80,
+        left: 180,
+        color: COLORS.white,
+        fontSize: 80,
+        transform: [
+            {
+                translateX: 0
+            }
+        ]
+    },
+    logoImage: {
+        left: 96,
+        top: -23,
+        width: 82,
+        height: 82,
+        resizeMode: 'contain',
+        transform: [
+            {
+                translateX: 0
+            }
+        ]
+    },
     subTitle: {
-		color: COLORS.white,
-		fontSize: 20
-	},
+        color: COLORS.white,
+        fontSize: 20
+    },
     centeredContainer: {
         flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-
+        justifyContent: 'center',
+        alignItems: 'center'
     },
-	rectangle: {
-		width: screenWidth,
-		height: screenHeight * heightPercentage,
-		top: 20,
-		borderTopLeftRadius: 23,
-		borderTopRightRadius: 23,
-		borderBottomLeftRadius: 0,
-		borderBottomRightRadius: 0,
-		backgroundColor: '#FFF',
-		marginBottom: 0, // Adjust to properly stack above the main content
+    rectangle: {
+        width: screenWidth,
+        height: screenHeight * heightPercentage,
+        top: 20,
+        borderTopLeftRadius: 23,
+        borderTopRightRadius: 23,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+        backgroundColor: '#FFF',
+        marginBottom: 0, // Adjust to properly stack above the main content
+    },
+	bottomCenteredContainer: {
+		position: 'absolute',
+        bottom: 40,
+        left: 0,
+        right: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
 	}
 })
 
