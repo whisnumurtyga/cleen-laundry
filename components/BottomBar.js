d15ab82142799c5a9a33bf497dbe2c350ecfd55a
Whisnumurty Galih Ananta <whisnumurty.galih.21@student.if.ittelkom-sby.ac.id>
2023-12-26 20:53:41 +0700

    tambahkan component bottombar

--- components/BottomBar.js
+++ components/BottomBar.js
@@ -1,40 +1,59 @@
 import React from "react";
-import { View, TouchableOpacity, StyleSheet, Image, Dimensions } from "react-native";
+import { View, TouchableOpacity, StyleSheet, Image, Dimensions, Text } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons"; // Menggunakan FontAwesome5 sebagai contoh ikon, Anda dapat mengganti dengan ikon lain
+import { useNavigation } from "@react-navigation/native"; // import useNavigation dari React Navigation
 
 const { width } = Dimensions.get('window'); // Mendapatkan lebar layar
 
 
 const BottomBar = ({ onHomePress, onOrderPress, onChatPress }) => {
-    
+    const navigation = useNavigation();
+
+    const navigateToHome = () => {
+        // Navigasi ke layar Home saat tombol Home ditekan
+        navigation.navigate('Home');
+    };
+
+    const navigateToOrder = () => {
+        // Navigasi ke layar Order saat tombol Order ditekan
+        navigation.navigate('Order');
+    };
+
+    const navigateToChat = () => {
+        // Navigasi ke layar Chat saat tombol Chat ditekan
+        navigation.navigate('Chat');
+    };
     return (
         <View style={styles.container}>
             <TouchableOpacity
-                onPress={onHomePress}
+                onPress={navigateToHome}
                 style={styles.iconContainer}
             >
                 <Image
                     source={require("../assets/bottom-bar/home.png")}
                     style={styles.iconImage}
                 />
+                <Text>Home</Text>
             </TouchableOpacity>
             <TouchableOpacity
-                onPress={onOrderPress}
+                onPress={navigateToOrder}
                 style={styles.iconContainer}
             >
                 <Image
                     source={require("../assets/bottom-bar/order.png")}
                     style={styles.iconImage}
                 />
+                <Text>Order</Text>
             </TouchableOpacity>
             <TouchableOpacity
-                onPress={onChatPress}
+                onPress={navigateToChat}
                 style={styles.iconContainer}
             >
                 <Image
                     source={require("../assets/bottom-bar/chat.png")}
                     style={styles.iconImage}
                 />
+                <Text>Chat</Text>
             </TouchableOpacity>
         </View>
     );
@@ -51,7 +70,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         paddingVertical: 10,
         width: {width},     
-        top: 375,
+        top: 365,
       },
       iconContainer: {
         alignItems: 'center',
