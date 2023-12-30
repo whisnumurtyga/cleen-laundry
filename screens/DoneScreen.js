c51b72650284040a0ef9617107487037f8917be5
satrioabdi <44046213+satrioabdi@users.noreply.github.com>
2023-12-31 00:03:02 +0700

    Add files via upload

--- screens/DoneScreen.js
+++ screens/DoneScreen.js
@@ -1,14 +1,136 @@
-// HistoryOrderScreen.js
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
 const DoneScreen = () => {
+  // Data transaksi (contoh sementara)
+  const transactions = [
+    {
+      totalAmount: 65000,
+      paymentMethod: 'E-wallet',
+      transactionCode: 'EWL7689Z',
+      transactionDate: new Date().toLocaleDateString(),
+    },
+    {
+      totalAmount: 145000,
+      paymentMethod: 'Debit Card',
+      transactionCode: 'DBC5678X',
+      transactionDate: new Date().toLocaleDateString(),
+    },
+    {
+      totalAmount: 150000,
+      paymentMethod: 'E-wallet',
+      transactionCode: 'EFG5678Z',
+      transactionDate: new Date().toLocaleDateString(),
+    },
+    {
+      totalAmount: 89000,
+      paymentMethod: 'Debit Card',
+      transactionCode: 'KLM9012X',
+      transactionDate: new Date().toLocaleDateString(),
+    },
+    {
+      totalAmount: 110000,
+      paymentMethod: 'Credit Card',
+      transactionCode: 'PQR3456A',
+      transactionDate: new Date().toLocaleDateString(),
+    },
+    {
+      totalAmount: 98000,
+      paymentMethod: 'Cash',
+      transactionCode: 'MNO7890B',
+      transactionDate: new Date().toLocaleDateString(),
+    },
+    {
+      totalAmount: 200000,
+      paymentMethod: 'Bank Transfer',
+      transactionCode: 'XYZ1234C',
+      transactionDate: new Date().toLocaleDateString(),
+    },
+  ];
+  
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Done Screen</Text>
-      {/* Tambahan elemen atau komponen lain yang diperlukan */}
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      {transactions.map((transaction, index) => (
+        <View style={styles.dataBox} key={index}>
+          <View style={styles.upperLeft}>
+            <Text style={styles.label}>Laundry Time</Text>
+            <Text style={styles.value}>Dry Clean</Text>
+            <Text style={styles.amountText}>{`Rp. ${transaction.totalAmount.toLocaleString()}`}</Text>
+          </View>
+          <View style={styles.upperRight}>
+            <Text style={[styles.value, styles.transactionCode]}>{transaction.transactionCode}</Text>
+            <Text style={[styles.value, styles.transactionDate]}>{transaction.transactionDate}</Text>
+          </View>
+        </View>
+      ))}
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 40,
+  },
+  dataBox: {
+    position: 'relative',
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    padding: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    width: '100%',
+    marginBottom: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  upperLeft: {
+    flex: 1,
+  },
+  upperRight: {
+    position: 'relative',
+    alignItems: 'flex-end',
+  },
+  lowerLeft: {
+    flex: 1,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  value: {
+    fontSize: 14,
+  },
+  amountText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 15,
+    marginBottom: 10, // Menambah jarak bawah
+  },
+  transactionCode: {
+    fontSize: 14,
+    position: 'absolute',
+    top: 15,
+    right: 15,
+  },
+  transactionDate: {
+    fontSize: 14,
+    position: 'absolute',
+    bottom: 15, // Menempatkan di bawah
+    right: 15, // Menempatkan ke kanan
+  },
+});
 
 export default DoneScreen;
--- screens/ScreenProcess.js
+++ screens/ScreenProcess.js
@@ -1,62 +1,111 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import DriverScreen from './DriverScreen';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
-const ProcessScreen = ({ route }) => {
-  const { totalAmount, paymentMethod, transactionCode, transactionDate } = route.params || {};
-  const navigation = useNavigation();
+const ProcessScreen = () => {
+  const [transactions, setTransactions] = useState([]);
 
-  const DriverScreen = () => {
-    // Ganti 'AnotherScreen' dengan nama screen yang ingin Anda navigasi
-    navigation.navigate('DriverScreen');
-  };
+  useEffect(() => {
+    // Contoh data transaksi, Anda bisa menggantinya dengan data sebenarnya
+    const sampleTransactions = [
+      {
+        totalAmount: 69000,
+        paymentMethod: 'Cash',
+        transactionCode: 'IAX8918W',
+        transactionDate: new Date().toLocaleDateString(),
+      },
+      {
+        totalAmount: 120000,
+        paymentMethod: 'Credit Card',
+        transactionCode: 'XZB1234Y',
+        transactionDate: new Date().toLocaleDateString(),
+      },
+      // ... tambahkan data transaksi lain di sini jika perlu
+    ];
+
+    setTransactions(sampleTransactions);
+  }, []);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.dataBox}>
-        <View style={styles.dataRow}>
-          <Text style={styles.label}>Total Amount:</Text>
-          <Text style={styles.value}>{totalAmount}</Text>
+    <ScrollView contentContainerStyle={styles.container}>
+      {transactions.map((transaction, index) => (
+        <View style={styles.dataBox} key={index}>
+          <View style={styles.upperLeft}>
+            <Text style={styles.label}>Laundry Time</Text>
+            <Text style={styles.value}>Dry Clean</Text>
+            <Text style={styles.amountText}>{`Rp. ${transaction.totalAmount.toLocaleString()}`}</Text>
+          </View>
+          <View style={styles.upperRight}>
+            <Text style={[styles.value, styles.transactionCode]}>{transaction.transactionCode}</Text>
+            <Text style={[styles.value, styles.transactionDate]}>{transaction.transactionDate}</Text>
+          </View>
         </View>
-        <View style={styles.dataRow}>
-          <Text style={styles.label}>Payment Method:</Text>
-          <Text style={styles.value}>{paymentMethod}</Text>
-        </View>
-        <View style={styles.dataRow}>
-          <Text style={styles.label}>Transaction Code:</Text>
-          <Text style={styles.value}>{transactionCode}</Text>
-        </View>
-        <View style={styles.dataRow}>
-          <Text style={styles.label}>Transaction Date:</Text>
-          <Text style={styles.value}>{transactionDate}</Text>
-        </View>
-      </View >
-
-      {/* Tombol Navigasi ke Screen Lain */}
-      <View style={{paddingTop: 350}}>
-      <TouchableOpacity onPress={DriverScreen} style={styles.button}>
-        <Text style={styles.buttonText}>Lacak Driver</Text>
-      </TouchableOpacity>
-      </View>
-    </View>
+      ))}
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  // ... (kode gaya yang sudah ada)
-
-  button: {
-    marginTop: 20,
-    backgroundColor: '#2E6B60',
-    padding: 10,
-    borderRadius: 5,
+  container: {
+    flexGrow: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 40,
   },
-  buttonText: {
-    color: 'white',
+  dataBox: {
+    position: 'relative',
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    padding: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    width: '100%',
+    marginBottom: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  upperLeft: {
+    flex: 1,
+  },
+  upperRight: {
+    position: 'relative',
+    alignItems: 'flex-end',
+  },
+  lowerLeft: {
+    flex: 1,
+  },
+  label: {
     fontSize: 16,
     fontWeight: 'bold',
-    textAlign: 'center',
+    marginBottom: 5,
+  },
+  value: {
+    fontSize: 14,
+  },
+  amountText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 15,
+    marginBottom: 10, // Menambah jarak bawah
+  },
+  transactionCode: {
+    fontSize: 14,
+    position: 'absolute',
+    top: 15,
+    right: 15,
+  },
+  transactionDate: {
+    fontSize: 14,
+    position: 'absolute',
+    bottom: 15, // Menempatkan di bawah
+    right: 15, // Menempatkan ke kanan
   },
 });
 
